837865605c6f53fcb332481890c378312d2f7a3a
Yefry Sanchez <yefrymsp@gmail.com>
2024-08-31 15:40:59 -0400

    comment CRUD

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -9,7 +9,7 @@ commentRouter.get("/", async (req, res) => {
   res.status(200).json(comment);
 });
 
-/// Post Create comment
+/// Post/ Create comment
 
 commentRouter.post("/", async (req, res) => {
   const { comment, postId, userName, email } = req.body;
@@ -23,6 +23,41 @@ commentRouter.post("/", async (req, res) => {
 
     //Comment create notification
     res.status(201).json({ msg: "Comment created" });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+/// Put/  Edit comment
+
+commentRouter.put("/", async (req, res) => {
+  const { _id, comment } = req.body;
+  try {
+    if (!_id || !comment) {
+      return res.status(400).json({ msg: "Error, please try again." });
+    }
+    //Comment edit
+    const findComment = await Comment.updateOne({ _id }, {$set: {comment}});
+   
+    //Comment create notification
+    res.status(201).json({ msg: "Comment updated" });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+/// Delete/  delete comment
+
+commentRouter.delete("/", async (req, res) => {
+  const { _id } = req.body;
+  try {
+    if (!_id) {
+      return res.status(400).json({ msg: "Error, please try again." });
+    }
+    //Comment deleted
+    const comment = await Comment.deleteOne({ _id });
+    //Comment create notification
+    res.status(201).json({ msg: "Comment deleted" });
   } catch (error: any) {
     res.status(400).json({ error: error.message });
   }
